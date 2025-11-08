83452ceaabc3ed644fca78d99be4c3afbb2ecf9c
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-08 12:34:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // This value is only used server-side – safe to commit
     WEBHOOK_SECRET: "alphastream-bot-secure-2025!x7k9",
   },
 };
