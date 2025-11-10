4156542e1d25c4f4903f7fa03e0e44e98c5125f4
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-10 11:41:09 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import "./globals.css";
 import "../components/LiveDashboard";
-import LiveDashboard from "../components/LiveDashboar";
+import LiveDashboard from "../components/LiveDashboard";
 
 export const metadata = {
   title: "AlphaStream v3.1",
