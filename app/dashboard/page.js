6c99169ca9d7f8bb299b3ceb3cc3487f9dc8bb10
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-10 12:58:05 -0500

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,9 @@
 // app/dashboard/page.js
-import LiveDashboard from "@/components/LiveDashboard";
+import LiveDashboard from '@/components/LiveDashboard';
 
 export default function Dashboard() {
   return <LiveDashboard />;
 }
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 export const revalidate = 0;
