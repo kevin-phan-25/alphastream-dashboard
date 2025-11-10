5c67b494eaa18d222d2bf923f3f230c743283823
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-10 12:45:41 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true
+  },
   env: {
     WEBHOOK_SECRET: "alphastream-bot-secure-2025!x7k9",
   },
