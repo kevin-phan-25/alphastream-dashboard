261c54b9d53a4153ab7e34a8acfd0308eb59217d
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-11 05:56:14 -0500

    Delete app directory

--- app/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-// app/dashboard/page.js
-import LiveDashboard from '@/components/LiveDashboard';
-
-export default function Dashboard() {
-  return <LiveDashboard />;
-}
-
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>AlphaStream Dashboard v16.2</h1>
-      <p>Waiting for webhook data...</p>
-      {/* Your existing dashboard UI here */}
-    </main>
-  );
-}
