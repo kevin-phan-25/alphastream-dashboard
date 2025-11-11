f3456b41ddf730ae8695ae671bc1d8cd2cf71a50
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-11 05:58:07 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: '#10b981',
+        red: '#ef4444',
+        gray: '#1e293b',
+      },
+    },
+  },
+  plugins: [],
+};
