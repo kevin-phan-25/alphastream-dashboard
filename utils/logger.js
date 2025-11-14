308fdef1f185dda078eb1c856f0eb59e3d0ee89c
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-14 15:24:23 -0500

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,12 @@
+import pool from '../db/pool.js';
+
+export const log = async (event, symbol = '', note = '') => {
+  try {
+    await pool.query(
+      'INSERT INTO logs (event, symbol, note) VALUES ($1, $2, $3)',
+      [event, symbol, note]
+    );
+  } catch (err) {
+    console.error('LOG ERROR:', err);
+  }
+};
