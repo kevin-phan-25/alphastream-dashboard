f4ccba8b3495d02c2c09168ed14369bc4cfc5420
kevin-phan-25 <zerotoitsuccess@gmail.com>
2025-11-16 03:58:15 -0500

    Delete api directory

--- api/close.js
+++
@@ -1,3 +0,0 @@
-// api/close.js
-import { closeAll } from '../services/alpaca.js';
-export default async (req, res) => { await closeAll(); res.json({ ok: true }); };
--- api/logs.js
+++
@@ -1,6 +0,0 @@
-// api/logs.js
-import pool from '../db/pool.js';
-export default async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM logs ORDER BY created_at DESC LIMIT 100');
-  res.json(rows);
-};
